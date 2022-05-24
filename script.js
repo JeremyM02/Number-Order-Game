5ea9c0bc826d75d7a8e45cee7f9516e5c40b85ed
JeremyM02 <j3r3mee20>
2022-05-24 08:50:15 -0700

    better stuff

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-const card = document.querySelector('.card');
+// const card = document.querySelector('.card');
+const cardList = document.querySelector('.cardList');
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+let hoverIndex = 0;
 
-const randomizeNumber = () => Math.floor(Math.random() * 100);
+const randomizeNumber = (min, max) => min + Math.floor(Math.random() * max);
 
-card.style.left = `${randomizeNumber()} px`;
-console.log('done1');
+
+const addCard = value => {
+    let card = document.createElement('div');
+    card.classList.add('card');
+    const cardWidth = card.offsetWidth;
+    card.innerHTML = value;
+
+    let randomXPos = randomizeNumber(0, screenWidth - cardWidth);
+    let randomYPos = randomizeNumber(0, screenHeight - 5 * cardWidth);
+
+    card.style.left = `${randomXPos}px`;
+    card.style.top = `${randomYPos}px`;
+    cardList.appendChild(card);
+
+    card.addEventListener('mouseover', function(){
+        card.style.zIndex = `${hoverIndex}`;
+        hoverIndex++;
+        console.log(hoverIndex);
+    })
+};
+
+console.log('Screen Width', screenWidth);
+console.log('Screen Height', screenHeight);
+
+for (var i = 1; i < 10; i++){
+    addCard(i);
+}
