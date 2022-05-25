eee649fc6bf2047e8c96c0f6d72a59a5a42b1d40
JeremyM02 <jeremyj0022@gmail.com>
2022-05-25 08:07:41 -0700

    Finished Game - With 3 difficulties

--- script.js
+++ script.js
@@ -1,41 +1,193 @@
-// const card = document.querySelector('.card');
-const cardList = document.querySelector('.cardList');
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
-let hoverIndex = 0;
+const cardWidth = 154;
+let currentNum = 1;
+let playGame = false;
 
-// const randomizeNumber = (min, max) => min + Math.floor(Math.random() * max);
-function randomizeNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+const cardList = document.querySelector('.cardList');
+const gameButton = document.getElementById('game-button');
+const gameText = document.getElementById('game-text');
+const resultText = document.getElementById('result-text');
+const topBar = document.querySelector('.topbar');
+const resetButton = document.getElementById('reset-game');
+
+const difficultyDiv = document.querySelector('.difficulty');
+const easy = document.getElementById('easy');
+const normal = document.getElementById('normal');
+const hard = document.getElementById('hard');
+let numCards;
+let difficulty;
+
+easy.addEventListener('click', function(){
+    gameStart('easy');
+    difficulty = 'easy';
+});
+normal.addEventListener('click', function(){
+    gameStart('normal');
+    difficulty = 'normal';
+});
+hard.addEventListener('click', function(){
+    gameStart('hard');
+    difficulty = 'hard';
+});
+
+resetButton.addEventListener('click', function(){
+    removeAllChildNodes(cardList);
+    resetButton.style.display = 'none';
+    gameButton.disabled = true;
+    topBar.style.left = '10px';
+    playGame = false;
+    currentNum = 1;
+
+    switch (difficulty){
+        case 'easy':
+            gameStart('easy');
+            break;
+
+        case 'normal':
+            gameStart('normal');
+            break;
+
+        case 'hard':
+            gameStart('hard');
+            break;
+    }
+})
+
+function gameStart(difficulty){
+    console.log('lets play!');
+    if (difficulty === 'easy'){
+        numCards = 7;
+        gameText.style.borderColor = 'green';
+
+    } else if (difficulty === 'normal') {
+        numCards = 10;
+
+    } else if (difficulty === 'hard'){
+        numCards = 15;
+        gameText.style.borderColor = 'red';
+    }
+    easy.remove();
+    normal.remove();
+    hard.remove();
+
+    gameText.textContent = 'Memorize the order of the cards!';
+
+    playCards();
 }
+
+function winGame(){
+    resultText.textContent = `Congratulations! You counted to ${numCards}!`;
+    resultText.style.display = "block";
+    resultText.style.color = "green";
+    setTimeout(function(){
+        resultText.style.opacity = '1';
+    }, 100)
+}
+
+function checkCorrect(num){
+    num = Number(num);
+    console.log('num:', num);
+    console.log('currentNum: ',currentNum);
+    if (num === currentNum){
+        if (currentNum === numCards){
+            winGame();
+        } else {
+            currentNum++;
+            gameText.textContent = `Click the cards in order from 1 to ${numCards}! (Current Number: ${currentNum})`;   
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
+
 
 const addCard = value => {
     let card = document.createElement('div');
     card.classList.add('card');
-    const cardWidth = card.offsetWidth;
     card.innerHTML = value;
+    card.id = value;
 
     let randomXPos = randomizeNumber(0, screenWidth - cardWidth);
-    let randomYPos = randomizeNumber(0, screenHeight - 5 * cardWidth);
-
-    console.log(i, randomXPos);
+    let randomYPos = randomizeNumber(90, screenHeight - cardWidth);
 
     card.style.left = `${randomXPos}px`;
     card.style.top = `${randomYPos}px`;
     cardList.appendChild(card);
 
-    card.addEventListener('mouseover', function(){
-        card.style.zIndex = `${hoverIndex}`;
-        hoverIndex++;
-        console.log(hoverIndex);
+    card.addEventListener('click', function(){
+        if (playGame === true){
+            let isCorrect = checkCorrect(card.id);
+            if (isCorrect === true){
+                console.log('CORRECT');
+                card.style.backgroundColor = 'green';   
+                card.textContent = card.id;
+
+                if (Number(card.id) != numCards) {
+                    card.style.opacity = '0';
+                    card.style.pointerEvents = 'none';
+                    setTimeout(function(){
+                        card.remove();
+                    
+                    }, 1000);
+                }
+            } else {
+                console.log('NOT CORRECT');
+                card.style.backgroundColor = 'red';
+                card.textContent = card.id;
+                card.style.zIndex = '2';
+                playGame = false;
+
+                const correctCard = document.getElementById(currentNum);
+                correctCard.style.backgroundColor = 'gray';
+                correctCard.textContent = currentNum;
+                correctCard.style.zIndex = '3';
+
+                resetButton.style.display = 'block';
+            }
+        }
     })
 };
 
 console.log('Screen Width', screenWidth);
 console.log('Screen Height', screenHeight);
 
-for (var i = 1; i < 10; i++){
-    addCard(i);
+function playCards(){
+    let i = numCards
+    gameButton.style.display = "block";
+
+    let addCardsInterval = setInterval(() => {
+        addCard(i);
+        i--;
+        if (i === 0){
+            clearInterval(addCardsInterval);
+            gameButton.disabled = false;
+            gameButton.textContent = 'Ready!';
+            gameButton.addEventListener('click', function(){    
+                gameButton.style.display = 'none';
+                topBar.style.left = '0';
+                gameText.textContent = `Click the cards in order from 1 to ${numCards}! (Current Number: ${currentNum})`;
+
+                for (let i = 0; i < cardList.children.length; i++){
+                    let card = cardList.children[i];
+                    card.textContent = '?';
+                    playGame = true;
+                }
+            })
+        }
+    }, 250);
+}
+
+//"Borrowed"
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function randomizeNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
